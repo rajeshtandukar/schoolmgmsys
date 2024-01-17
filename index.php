03e048eb5a5b6cd67eec77c26ad3378b98d90d5e
linux <rtandukar@gmial.com>
2024-01-17 23:00:11 +0545

    update index

--- index.php
+++ index.php
@@ -73,9 +73,9 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		#error_reporting(-1);
-		error_reporting(E_ALL & ~E_NOTICE  & ~E_DEPRECATED & ~E_WARNING);
-		#ini_set('display_errors', 1);
+		error_reporting(-1);
+		#error_reporting(E_ALL & ~E_NOTICE  & ~E_DEPRECATED & ~E_WARNING);
+		ini_set('display_errors', 1);
 	break;
 
 	case 'testing':
